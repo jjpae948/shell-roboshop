653148dd39d4bca659f5643d209f9ca3d25d2245
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-30 21:09:08 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 #export PATH=$PATH:/usr/local/bin
@@ -29,8 +28,7 @@ fi
 
 get_instance_id(){
     name=$1
-    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query
-    "Reservations[0].Instances[0].InstanceId" --output text
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
 }
 
 for instance in $@
@@ -54,5 +52,47 @@ do
         else
             echo "roboshop-$instance already running: $INSTANCE_ID"
         fi
+
+        # update R53 record
+        if [ $instance == "frontend" ]; then
+            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+            --query 'Reservations[*].Instances[*].PublicIpAddress' \
+            --output text
+            )
+            R53_RECORD="$DOMAIN_NAME"
+        else
+            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+            --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+            --output text
+            )
+            R53_RECORD="$instance.$DOMAIN_NAME"
+        fi
+
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+            {
+                "Comment": "Update A record to new IP",
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": "'$R53_RECORD'",
+                            "Type": "A",
+                            "TTL": 1,
+                            "ResourceRecords": [
+                                {
+                                    "Value": "'$IP'"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        '
+         echo "updated R53 record for: $instance"
+        else
+          echo "roboshop-$instance already running:$INSTANCE_ID"    
+        fi
     fi
-done        
+done
