ac353bfad21d7ae8d8b83934e55dbc72999eed8b
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-30 14:13:18 +0530

    api doc added

---
+++ roboshop-v2.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+AMI_ID= "ami-0220d79f3f480ecf5" 
+ZONE_ID= "Z01842473AEAVYOAVFX3C" #  replace with  your zone ID 
+DOMAIN_NAME="daws80s.space" # replace with your domain name
+R= "\e[31m"
+G= "\E[32m"
+Y= "\e[33m"
+N= "\e[0m"
+
+### validation ###
+if  [ $# -1t 2 ];then
+     echo -e $R ERROR:: Atleast 2 Arguments
+     echo "USAGE: $0 [create/delete] [instance2...]"
+     exit1
+fi     
