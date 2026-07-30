295ef94cf6de75512240a6cc4dae0414523fcc85
jjpae948 <parvathiyalapalli1105@gmail.com>
2026-07-30 20:08:23 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -26,8 +26,8 @@ fi
 
 get_instance_id(){
   name=$1
-  aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$instance" "Name=instance-state-name,Values=running" --query
-  "Reservations[0].Instance[0].InstanceId" --output text
+  aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query
+   "Reservations[0].Instance[0].InstanceId" --output text
 }
 
 for instance in $@
